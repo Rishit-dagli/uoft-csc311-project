7e1fe84bbdac24dedd2902828402e8240896e7d2
dqieu <tiendat691@gmail.com>
2024-03-28 10:16:03 -0400

    Part B

--- part_a/neural_network.py
+++ part_a/neural_network.py
@@ -6,10 +6,9 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.data
 
-import matplotlib.pyplot as plt
-
 import numpy as np
 import torch
+from scipy.sparse import hstack, csr_matrix
 
 
 def load_data(base_path="../data"):
@@ -36,7 +35,34 @@ def load_data(base_path="../data"):
     zero_train_matrix = torch.FloatTensor(zero_train_matrix)
     train_matrix = torch.FloatTensor(train_matrix)
 
+    student_meta = pd.read_csv(os.path.join(base_path, 'student_meta_clean.csv'))
+    question_meta = pd.read_csv(os.path.join(base_path, 'question_meta.csv'))
+    subject_meta = pd.read_csv(os.path.join(base_path, 'subject_meta.csv'))
+
+    student_meta_cleaned = process_student_metadata(student_meta)
+    question_meta_cleaned = process_question_metadata(question_meta, subject_meta)
+
     return zero_train_matrix, train_matrix, valid_data, test_data
+
+def combine_metadata_with_sparse_data(sparse_data, student_meta, question_meta, user_ids, question_ids):
+    """
+    Combine the student and question metadata with the given sparse dataset.
+
+    :param sparse_data: Sparse matrix representing the training data.
+    :param student_meta: DataFrame containing processed student metadata.
+    :param question_meta: DataFrame containing processed question metadata.
+    :param user_ids: List or array of user IDs corresponding to the rows in sparse_data.
+    :param question_ids: List or array of question IDs corresponding to the columns in sparse_data.
+    :return: Combined sparse dataset.
+    """
+    # Convert metadata DataFrames to sparse format
+    student_sparse = csr_matrix(student_meta.loc[user_ids].values)
+    question_sparse = csr_matrix(question_meta.loc[question_ids].values)
+
+    # Append metadata to the sparse data
+    combined_sparse_data = hstack([sparse_data, student_sparse, question_sparse])
+
+    return combined_sparse_data
 
 
 class AutoEncoder(nn.Module):
@@ -80,9 +106,7 @@ class AutoEncoder(nn.Module):
         return out
 
 
-def train(
-    model, lr, lamb, train_data, zero_train_data, valid_data, num_epoch, verbose=False
-):
+def train(model, lr, lamb, train_data, zero_train_data, valid_data, num_epoch):
     """Train the neural network, where the objective also includes
     a regularizer.
 
@@ -95,7 +119,7 @@ def train(
     :param num_epoch: int
     :return: None
     """
-    # TODO: Add a regularizer to the cost function.
+
 
     # Tell PyTorch you are training the model.
     model.train()
@@ -104,14 +128,8 @@ def train(
     optimizer = optim.SGD(model.parameters(), lr=lr)
     num_student = train_data.shape[0]
 
-    validation_accuracy = []
-    training_loss = []
-    validation_loss = []
-
     for epoch in range(0, num_epoch):
         train_loss = 0.0
-        correct = 0.0
-        total = 0.0
 
         for user_id in range(num_student):
             inputs = Variable(zero_train_data[user_id]).unsqueeze(0)
@@ -120,7 +138,7 @@ def train(
             optimizer.zero_grad()
             output = model(inputs)
 
-            reg_term = lamb * model.get_weight_norm()
+            reg_term = lamb/2 * model.get_weight_norm()
 
             # Mask the target to only compute the gradient of valid entries.
             nan_mask = np.isnan(train_data[user_id].unsqueeze(0).numpy())
@@ -133,24 +151,10 @@ def train(
             optimizer.step()
 
         valid_acc = evaluate(model, zero_train_data, valid_data)
-
-        model.eval()
-        valid_loss = 0.0
-        for i, u in enumerate(valid_data["user_id"]):
-            inputs = Variable(zero_train_data[u]).unsqueeze(0)
-            output = model(inputs)
-            guess = output[0][valid_data["question_id"][i]].item()
-            valid_loss += (guess - valid_data["is_correct"][i]) ** 2
-
-        if verbose:
-            print(
-                "Epoch: {} \tTraining Cost: {:.6f}\t "
-                "Valid Acc: {}".format(epoch, train_loss, valid_acc)
-            )
-        training_loss.append(train_loss)
-        validation_loss.append(valid_loss)
-        validation_accuracy.append(valid_acc)
-    return training_loss, validation_loss, validation_accuracy
+        print(
+            "Epoch: {} \tTraining Cost: {:.6f}\t "
+            "Valid Acc: {}".format(epoch, train_loss, valid_acc)
+        )
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -191,13 +195,11 @@ def main():
     # validation set.                                                   #
     #####################################################################
 
-    print("Start hyperparameter tuning for part (b).")
-    print()
-
+    # Possible values of hyperparameters to try
     k_values = [10, 50, 100, 200, 500]
-    lr_values = [0.01, 0.03, 0.05]
-    epoch_values = [10, 20]
-    lamb = 0.0
+    lr_values = [0.001, 0.01, 0.05]
+    epoch_values = [10, 20, 30]
+    lamb = 0.001
 
     best_config = {
         "k": k_values[0],
@@ -225,10 +227,14 @@ def main():
                     num_epoch,
                 )
 
-                valid_acc = evaluate(model, zero_train_matrix, valid_data)
+                # Evaluate the model on the validation set
+                valid_acc = evaluate(
+                    model, zero_train_matrix, valid_data
+                )  # Assume this function is implemented
 
-                # print(f"Validation accuracy: {valid_acc}")
+                print(f"Validation accuracy: {valid_acc}")
 
+                # Update the best configuration if the current model is better
                 if valid_acc > best_config["valid_acc"]:
                     best_config.update(
                         {"k": k, "lr": lr, "epochs": num_epoch, "valid_acc": valid_acc}
@@ -236,81 +242,6 @@ def main():
 
     print(
         f"Best configuration: k={best_config['k']}, lr={best_config['lr']}, epochs={best_config['epochs']} with validation accuracy: {best_config['valid_acc']}"
-    )
-
-    print()
-    print("Start part (c)")
-    print()
-
-    N_valid = np.count_nonzero(valid_data["is_correct"])
-    N_train = np.count_nonzero(zero_train_matrix)
-
-    model = AutoEncoder(num_question=train_matrix.shape[1], k=best_config["k"])
-    lamb = 0.0
-    train_losses, valid_losses, valid_acc = train(
-        model,
-        best_config["lr"],
-        lamb,
-        train_matrix,
-        zero_train_matrix,
-        valid_data,
-        best_config["epochs"],
-    )
-
-    test_accuracy = evaluate(model, zero_train_matrix, test_data)
-    print(f"Test accuracy: {test_accuracy}")
-
-    epochs = range(1, len(train_losses) + 1)
-    plt.figure()
-    plt.plot(epochs, np.array(train_losses) / N_train, label="Training loss")
-    plt.plot(epochs, np.array(valid_losses) / N_valid, label="Validation loss")
-    plt.title("Training and Validation Loss")
-    plt.xlabel("Epochs")
-    plt.ylabel("Loss")
-    plt.legend()
-    plt.savefig("part3c.png")
-
-    print("Start hyperparameter tuning for part (d)")
-    print()
-
-    lamb = [1e-3, 1e-2, 1e-1, 1]
-    best_config_l = {
-        "lamb": lamb[0],
-        "valid_acc": -float("inf"),
-    }
-
-    for l in lamb:
-        print(f"Training with lambda={l}")
-
-        model = AutoEncoder(num_question=train_matrix.shape[1], k=best_config["k"])
-        train(
-            model,
-            best_config["lr"],
-            l,
-            train_matrix,
-            zero_train_matrix,
-            valid_data,
-            best_config["epochs"],
-        )
-
-        valid_acc = evaluate(model, zero_train_matrix, valid_data)
-
-        if valid_acc > best_config_l["valid_acc"]:
-            best_config_l.update({"lamb": l, "valid_acc": valid_acc})
-
-    model = AutoEncoder(num_question=train_matrix.shape[1], k=best_config["k"])
-    train(
-        model,
-        best_config["lr"],
-        best_config_l["lamb"],
-        train_matrix,
-        zero_train_matrix,
-        valid_data,
-        best_config["epochs"],
-    )
-    test_accuracy = evaluate(model, zero_train_matrix, test_data)
-    print(
-        f"Best configuration: k={best_config['k']}, lr={best_config['lr']}, epochs={best_config['epochs']}, lambda={best_config_l['lamb']} with validation accuracy: {best_config_l['valid_acc']} and test accuracy: {test_accuracy}"
     )
     #####################################################################
     #                       END OF YOUR CODE                            #
