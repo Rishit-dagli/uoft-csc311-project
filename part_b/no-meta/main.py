76427537fe4d54c75faa81a734752c19e54b56de
Rishit Dagli <rishit.dagli@gmail.com>
2024-03-29 21:26:53 +0000

    use rounding

--- part_b/no-meta/main.py
+++ part_b/no-meta/main.py
@@ -219,7 +219,7 @@ class QuestionUserModel(pl.LightningModule):
         loss = self.loss_fn(predictions, labels)
         preds = torch.sigmoid(predictions)
         self.log("test_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
-        np.savez("test_preds.npz", preds=preds.detach().cpu().numpy())
+        np.savez("test_preds.npz", preds=torch.round(preds).detach().cpu().numpy())
         np.savez("test_labels.npz", labels=labels.detach().cpu().numpy())
         self.accuracy(preds, labels.int())
         self.log("test_acc", self.accuracy, on_step=False, on_epoch=True, prog_bar=True)
